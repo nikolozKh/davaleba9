f80923951d279e2c9135195fbf63ac1728fd01d1
nika <nikolozkharebashvili2009@gmail.com>
2025-04-12 18:52:25 +0400

    Add project files.

---
+++ davaleba9/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace davaleba9
+{
+    internal class Employee
+    {
+        private string _name;
+        private string _email;
+        private string _phone;
+        private string _job;
+        private double _salary;
+
+        public string Name{ get { return _name; } }
+        public string Email{ get { return _email; } }
+        public string Phone{ get { return _phone; } }
+        public string Job{ get { return _job; } set { _job = value; } }
+        public double Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+
+
+        }
+
+        public Employee()
+        {
+            
+        }
+        public Employee(string name, string email, string phone, string job, double salary)
+        {
+            _name = name;
+            _email = email;
+            _phone = phone;
+            _job = job;
+            _salary = salary;
+
+        }
+
+        public void print()
+        {
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine($"Email: {_email}");
+            Console.WriteLine($"Phone: {_phone}");
+            Console.WriteLine($"Job: {_job}");
+            Console.WriteLine($"Salary: {_salary}");
+        }
+
+    }
+}
---
+++ davaleba9/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+//1 ,2,3 მერვე დავალებაში გვქონდა და აღარ დავწერე
+
+using davaleba9;
+
+Employee employee = new Employee("nika", "example@gmail.com", "555565656", "anyjob", 5000);
+employee.print();
+Console.WriteLine();
+employee.Job = "newjob";
+employee.Salary = 6000;
+employee.print();
